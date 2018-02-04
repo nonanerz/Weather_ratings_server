025b41dadd612f557501207738b1172873b788f6
YarmolenkoDany <yarmolenko.dany@gmail.com>
2018-02-04 16:04:46 +0700

    refactoring code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express')
 var router = express.Router()
 var adminCredentials = require('../credentials/admin')
-var isAdmin = false
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(33333, req.cookies.admin)
   if (req.cookies.admin === 'true') {
     res.render('pages/adminPanel', { title: 'Admin panel' })
   } else {
@@ -13,7 +11,6 @@ router.get('/', function(req, res, next) {
 })
 
 router.post('/', function(req, res, next) {
-  console.log(2222, req.cookies)
   if (req.cookies.admin === 'true') {
     res.render('pages/adminPanel', { title: 'Admin panel' })
   } else {
@@ -24,7 +21,6 @@ router.post('/', function(req, res, next) {
 router.post('/login', function(req, res, next) {
   var data = req.body
   if (data.name === adminCredentials.name && data.password === adminCredentials.password) {
-    isAdmin = true
     res.cookie('admin', 'true')
     res.redirect('/')
   } else {
@@ -33,12 +29,10 @@ router.post('/login', function(req, res, next) {
 })
 
 router.get('/login', function(req, res, next) {
-  console.log(isAdmin)
   res.render('pages/login', { title: 'Sign in', isAdmin: req.cookies.admin })
 })
 
 router.post('/logout', function(req, res, next) {
-  isAdmin = false
   res.cookie('admin', 'false')
   res.redirect('/login')
 })
