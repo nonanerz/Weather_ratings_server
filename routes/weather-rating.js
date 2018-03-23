da12eb16e9dabb2e3675027ec0ed78a42f9c05a3
Bulava Eduard <bulavaeduard@gmail.com>
2018-03-23 17:58:48 +0200

    cs fix

--- routes/weather-rating.js
+++ routes/weather-rating.js
@@ -49,4 +49,48 @@ router.get('/:city', async function (req, res, next) {
   res.json({resources})
 })
 
+router.get('/:city/:id', async function (req, res, next) {
+  let resource = await Resource.findOne({_id: req.params.id})
+    .then(resource => resource)
+
+  let rating
+  if (req.params.city !== 'any') {
+    rating = await Rating.aggregate([
+      {
+        $match: {'resource': resource._id.toString(), 'city': req.params.city}},
+      {
+        $group: {
+          _id: null,
+          average_transaction_amount: {
+            $avg: '$rating'
+          },
+
+          'count': { $sum: 1 }
+
+        }
+      }
+    ])
+  } else {
+    rating = await Rating.aggregate([
+      {
+        $match: {'resource': resource._id.toString()}},
+      {
+        $group: {
+          _id: null,
+          average_transaction_amount: {
+            $avg: '$rating'
+          },
+
+          'count': { $sum: 1 }
+
+        }
+      }
+    ])
+  }
+
+  resource.rating = rating
+
+  res.json({resource: resource})
+})
+
 module.exports = router
