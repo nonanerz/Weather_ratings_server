bd8f76eef4c888355c60f1fb0483751cbf7af028
Bulava Eduard <bulavaeduard@gmail.com>
2018-03-24 20:57:35 +0200

    add pagination comments

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,13 @@ const Comment = require('../model/comment/model')
 const router = express.Router()
 
 router.get('/:id', async function (req, res, next) {
-    Comment.find({resource: req.params.id}).then(comments => {
+    let page = req.query.page ? req.query.page : 1;
+    let limit = req.query.limit ? req.query.limit : 5;
+    let offset = (page - 1) * limit;
+
+    Comment.find({resource: req.params.id}).limit(limit)
+        .skip(offset)
+        .then(comments => {
     res.json({comments})
   })
     .catch(next)
